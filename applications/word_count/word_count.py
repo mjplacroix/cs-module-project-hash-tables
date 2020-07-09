7da579175e998bee9f49e066ec12e935d9117380
Michael LaCroix <michael.jp.lacroix@gmail.com>
2020-07-09 12:14:40 -0400

    passing most word_count tests

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,32 @@
 def word_count(s):
     # Your code here
+    
+    # make string all lowercase
+    s = s.lower()
+    # split up into a list
+    s = s.split()
+    # remove none alpha characters
+    for w in range(len(s)):
+        for l in s[w]:
+            if l == "'":
+                continue
+            elif l.isalpha() == False:
+                s[w] = s[w].replace(l, "")
+
+
+    # split up into a list
+    # if already in dictionary, increment value
+    word_count = {}
+    for word in s:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+
+    print(word_count)
+    return word_count
+    # else create a dictionary for each new occurance
+    # return dicdtionary
 
 
 
